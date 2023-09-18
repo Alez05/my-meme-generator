ce2457938cdfabcaf34285c25faff32d120ce1d6
alez05 <cercel.alexandru436@gmail.com>
2023-09-18 23:49:01 +0100

    update

---
+++ meme-generator-react/src/component/myapp.jsx
@@ -0,0 +1,15 @@
+import Header from './header';
+import Meme from './meme';
+import Test from './test';
+
+const MyApp = () => {
+  return (
+    <>
+      <Header />
+      <Meme />
+      <Test />
+    </>
+  );
+};
+
+export default MyApp;
