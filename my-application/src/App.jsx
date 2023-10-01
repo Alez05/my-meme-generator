62f3632e7ddb7ec8ff0169fe633f280e2d869c13
alez05 <cercel.alexandru436@gmail.com>
2023-10-01 13:17:16 +0100

    versiune noua de cod

--- my-application/src/App.jsx
+++ my-application/src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react';
 
-function Toggle() {
-  function handleClick() {
-    // to do
-  }
+// make the state logic reusable
+const useToggle = () => {
+  const [isOn, setIsOn] = useState(true);
 
-  return <button>ON</button>;
-}
+  const click = () => {
+    setIsOn((s) => !s);
+  };
+
+  return [isOn, click];
+};
+
+const getLabelAndStyle = (bool) => {
+  const label = bool ? 'ON' : 'OFF';
+  const color = bool ? 'blue' : 'red';
+  const style = { cursor: 'pointer', color };
+
+  return { label, style };
+};
+
+const Toggle = () => {
+  const [isOn, click] = useToggle();
+
+  const { style,  label} = getLabelAndStyle(isOn);
+
+  return (
+    <button
+      style={style}
+      onClick={click}
+    >
+      {label}
+    </button>
+  );
+};
 
 export default Toggle;
