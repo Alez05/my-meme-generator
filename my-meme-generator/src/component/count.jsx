7bdb4be295fc9fe26eab362e97851b73c636b8d6
alez05 <cercel.alexandru436@gmail.com>
2023-10-16 12:46:38 +0100

    update the bastard

--- my-meme-generator/src/component/count.jsx
+++
@@ -1,5 +0,0 @@
-// const Count = ({ number }) => {
-//   return <div className='counter-number'>{number}</div>;
-// };
-
-// export default Count;
---
+++ src/component/count.jsx
@@ -0,0 +1,5 @@
+const Count = ({ number }) => {
+  return <div className='counter-number'>{number}</div>;
+};
+
+export default Count;
